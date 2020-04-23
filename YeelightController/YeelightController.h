77df4e3388fca80cc5014723eb4c1f335c3f77ef
Keith Lee <lee.mingkeith@gmail.com>
2020-04-24 00:44:28 +0900

    Initial Commit

---
+++ YeelightController/YeelightController.h
@@ -0,0 +1,13 @@
+//
+//  YeelightController.h
+//  YeelightController
+//
+//  Created by Keith Lee on 2020/04/24.
+//  Copyright © 2020 Keith Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YeelightController : NSObject
+
+@end
---
+++ YeelightController/YeelightController.m
@@ -0,0 +1,13 @@
+//
+//  YeelightController.m
+//  YeelightController
+//
+//  Created by Keith Lee on 2020/04/24.
+//  Copyright © 2020 Keith Lee. All rights reserved.
+//
+
+#import "YeelightController.h"
+
+@implementation YeelightController
+
+@end
